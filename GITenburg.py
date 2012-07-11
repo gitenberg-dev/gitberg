8fe1e5350652e65388cb66c36905e1c0b8fe6097
Seth Woodworth <seth@sethish.com>
2012-07-11 15:37:12 -0400

    generate a catalog from PG's xml index

--- GITenburg.py
+++ GITenburg.py
@@ -7,4 +7,10 @@ from filetypes import IGNORE_FILES
 from filetypes import MASTER_FILES
 
 def update_catalog():
-    mycat = rdfparse.Gutenberg(pickle_path, self.prefs.catalog)
+    pickle_path = './catalog.pickle'
+    mycat = rdfparse.Gutenberg(pickle_path)
+    success = mycat.updatecatalogue()
+    print success
+
+if __name__=='__main__':
+    update_catalog()

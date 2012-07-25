a0a7d4c91487df1b9576a6f0809dc044e541def7
Seth Woodworth <seth@sethish.com>
2012-07-25 19:47:56 +0000

    drying a dry run of metadata.yaml creation

--- GITenburg.py
+++ GITenburg.py
@@ -1,16 +1,91 @@
 #!/usr/bin/python
 """
 """
-import rdfparse
-from filetypes import AUDIO_FILES
-from filetypes import IGNORE_FILES
-from filetypes import MASTER_FILES
+import os
+import cPickle as pickle
+import yaml
 
-def update_catalog():
-    pickle_path = './catalog.pickle'
+import git
+import github3
+
+import rdfparse
+from filetypes import IGNORE_FILES
+
+from secrets import GH_USER
+from secrets import GH_PASSWORD
+
+PICKLE_PATH     = u'./catalog.pickle'
+ARCHIVE_ROOT    = u'/media/gitenberg'
+
+
+def update_catalog(pickle_path=PICKLE_PATH):
+    """ Use an imported repo to parse the Gutenberg XML index into a pickle 
+        This saves it to the file noted in PICKLE_PATH
+    """
     mycat = rdfparse.Gutenberg(pickle_path)
     success = mycat.updatecatalogue()
     print success
 
+def load_catalog(pickle_path=PICKLE_PATH):
+    """ Return catalog if local file exists, otherwise fetch it from gutenberg
+    """
+    if not os.path.isfile(pickle_path):
+        update_catalog(pickle_path)
+    return pickle.load(open(pickle_path, 'r'))
+
+def get_file_path(book):
+    """ Split a book's filename into a directory """
+    # Get the path to what our DB thinks the core file is (usually a .zip)
+    zip_path = os.path.join(ARCHIVE_ROOT, book.filename)
+    # work backwards and get the file
+    folder = os.path.split(zip_path)[0]
+    return folder
+
+def make_local_repo(folder):
+    """ Create a repo and add any file not in IGNORE_FILES """
+    # TODO: check if there is a .git subfolder already
+    repo = git.Repo.init(folder)
+    for file in repo.untracked_files:
+        file_type = os.path.splitext(file)[1]
+        if file_type not in IGNORE_FILES:
+            repo.index.add(file)
+        repo.index.commit("initial Project Gutenberg import")
+
+def get_team(org):
+    return org.list_teams()[0]
+
+def create_github_repo(title):
+    """ takes a github title, creates a repo under the GITenberg account """
+    gh = github3.login(username=GH_USER, password=GH_PASSWORD)
+    org = gh.organization(login='GITenberg')
+    team = org.list_teams()[0] # only one team in the github repo
+
+def create_metadata_yaml(book, folder):
+    """ Create a yaml metadata file that describes the repo
+        :book: rdfparse.Ebook instance
+        :folder: root folder of a git repo/book where the yaml file will be added
+    """
+    filename = 'metadata.yaml'
+    keys = ['lang', 'mdate', 'bookid', 'author', 'title', 'subj']
+    metadata = {}
+
+    for key in keys:
+        metadata[key] = getattr(book, key)
+
+    print os.path.join(folder, filename)
+    """
+    fp = file(os.path.join(folder, filename), 'w')
+    yaml.dump(metadata, fp, default_flow_style=False)
+    fp.close()
+    """
+    return True
+
 if __name__=='__main__':
-    update_catalog()
+    #update_catalog()
+    catalog = load_catalog()
+    for book in catalog:
+        print '\n'
+        print book.title
+        folder = get_file_path(book)
+        print folder
+        create_metadata_yaml

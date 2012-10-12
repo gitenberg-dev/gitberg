56ca38931579a8219715c693d6e5b64a34b9379a
Ian Cordasco <graffatcolmingov@gmail.com>
2012-10-12 09:21:29 -0400

    Closes #34.

--- GITenberg.py
+++ GITenberg.py
@@ -3,6 +3,7 @@
 """
 """
 import codecs
+import sys
 import os
 import cPickle as pickle
 import json
@@ -23,6 +24,7 @@ PICKLE_PATH     = u'./catalog.pickle'
 ARCHIVE_ROOT    = u'/media/gitenberg'
 
 import pdb
+from optparse import OptionParser
 
 
 def update_catalog(pickle_path=PICKLE_PATH):
@@ -33,12 +35,14 @@ def update_catalog(pickle_path=PICKLE_PATH):
     success = mycat.updatecatalogue()
     print success
 
+
 def load_catalog(pickle_path=PICKLE_PATH):
     """ Return catalog if local file exists, otherwise fetch it from gutenberg
     """
     if not os.path.isfile(pickle_path):
         update_catalog(pickle_path)
     return pickle.load(open(pickle_path, 'r'))
+
 
 def get_file_path(book):
     """ Split a book's filename into a directory """
@@ -48,11 +52,13 @@ def get_file_path(book):
     folder = os.path.split(zip_path)[0]
     return folder
 
+
 def git_add(file_name, folder):
     #git_add('exampleFile.txt', '/usr/local/example_git_repo_dir')
     cmd = ['git', 'add', file_name]
     p = subprocess.Popen(cmd, cwd=folder)
     p.wait()
+
 
 def git_commit(message, folder):
     #git_commit('exampleFile.txt', '/usr/local/example_git_repo_dir')
@@ -60,17 +66,20 @@ def git_commit(message, folder):
     p = subprocess.Popen(cmd, cwd=folder)
     p.wait()
 
+
 def git_add_remote_origin(remote, folder):
     #git_add_remote_origin(u'git@github.com:sethwoodworth/test.git', '/usr/local/example_git_repo_dir')
     cmd = ['git', 'remote', 'add', 'origin', remote]
     p = subprocess.Popen(cmd, cwd=folder)
     p.wait()
 
+
 def git_push_origin_master(folder):
     #git_add_remote_origin(u'git@github.com:sethwoodworth/test.git', '/usr/local/example_git_repo_dir')
     cmd = ['git', 'push', 'origin', 'master']
     p = subprocess.Popen(cmd, cwd=folder)
     p.wait()
+
 
 def make_local_repo(folder):
     """ Create a repo and add any file not in IGNORE_FILES """
@@ -91,6 +100,7 @@ def make_local_repo(folder):
     git_commit("Creating repo from Project Gutenberg import", folder)
     return repo
 
+
 def github_sanitize_string(book):
     """ Takes a string and sanitizes it for Github's url name format """
     _title = sub("[ ',]+", '-', book.title)
@@ -101,6 +111,7 @@ def github_sanitize_string(book):
     else:
         repo_title = "%s_%s" % (_title[:title_length], book.bookid)
     return repo_title
+
 
 def create_github_repo(book):
     """ takes a github title, creates a repo under the GITenberg account
@@ -114,7 +125,10 @@ def create_github_repo(book):
     repo_title = github_sanitize_string(book)
 
     try:
-        repo = org.create_repo(repo_title, description=_desc, homepage=u'http://GITenberg.github.com/', private=False, has_issues=True, has_wiki=False, has_downloads=True, team_id=int(team.id))
+        repo = org.create_repo(repo_title,
+                description=_desc, homepage=u'http://GITenberg.github.com/',
+                private=False, has_issues=True, has_wiki=False,
+                has_downloads=True, team_id=int(team.id))
     except github3.GitHubError as g:
         for error in g.errors:
             if 'message' in error and u'name already exists on this account' == error['message']:
@@ -126,6 +140,7 @@ def create_github_repo(book):
 
     print repo.html_url
     return repo
+
 
 def create_metadata_json(book, folder):
     """ Create a json metadata file that describes the repo
@@ -149,6 +164,7 @@ def create_metadata_json(book, folder):
         print "that file isn't in our local yet"
         return False
 
+
 def create_readme(book, folder, template):
     """ Create a readme file with book specific information """
     filename = 'README.rst'
@@ -169,12 +185,14 @@ def create_readme(book, folder, template):
     fp.close()
     return True
 
+
 def copy_files(folder):
     """ Copy the LICENSE and CONTRIBUTING files to each folder repo """
     files = [u'./LICENSE', u'./CONTRIBUTING.md']
     for file in files:
         shutil.copy(file, folder)
     return True
+
 
 def write_index(book, repo_url):
     """ append to an index file """
@@ -183,6 +201,7 @@ def write_index(book, repo_url):
     fp = open('./index.csv', 'a')
     fp.write(u"%s\t%s" % ( repo_url, book.bookid))
     fp.close()
+
 
 def do_stuff(catalog):
     count = 0
@@ -208,7 +227,115 @@ def do_stuff(catalog):
             git_push_origin_master(folder)
             write_index(book, repo.ssh_url)
 
+
+def upload_books(start, end):
+    assert start < end
+    for book in catalog[start:end]:
+        upload_book(book)
+
+
+def upload_book(book):
+    if 'right' in book.rights:
+        return
+    print('\n')
+    folder = get_file_path(book)
+    print("folder path:\t {0}".format(folder))
+    create_metadata_json(book, folder)
+    create_readme(book, folder, readme_template)
+    copy_files(folder)
+    make_local_repo(folder)
+    repo = create_github_repo(book)
+    git_add_remote_origin(repo.ssh_url, folder)
+    git_push_origin_master(folder)
+    write_index(book, repo.ssh_url)
+
+
+def run_tests():
+    return 0
+
+
+def delete_git_dirs(start, end):
+    assert start < end
+    for book in catalog[start:end]:
+        delete_git(get_file_path(book))
+
+
+def delete_git(folder):
+    pass
+
+
+def update_indices(start, end):
+    assert start < end
+    for book in catalog[start:end]:
+        update_git_index(get_file_path(book))
+
+
+def update_git_index(path):
+    pass
+
+
+def option_callback(opt_obj, opt_str, opt_val, parser):
+    """Callback function which handles some of the command-line options.
+
+    :param opt_obj: The actual Option Object that's created when calling
+        add_option on the parser. This gets sent as part of the callback.
+    :param opt_str: The option string, e.g., ``-U`` or ``--update-catalog``
+    :param opt_val: The value passed on the command-line.
+    :param parser: The parser itself (instance of OptionParser)
+    """
+    def parse_range(r):
+        r = r[1:-1].split(',')
+        # This ensures someone doesn't pass [100,200,300,etc]
+        return [int(v) for i, v in enumerate(r) if i < 2]
+
+    if opt_str in ('-U', '--update-catalog'):
+        if update_catalog():
+            sys.exit(0)
+        else:
+            sys.stderr.write('Error occurred updating catalog.')
+            sys.exit(127)
+    elif opt_str in ('-t', '--tests'):
+        sys.exit(run_tests())
+    elif opt_str in ('-r', '--run'):
+        upload_books(*parse_range(opt_str))
+    elif opt_str in ('-D', '--delete-git'):
+        delete_git_dirs(*parse_range(opt_str))
+
+
 if __name__=='__main__':
     #update_catalog()
     catalog = load_catalog()
-    do_stuff(catalog)
+    #do_stuff(catalog)
+    opts = OptionParser(usage='%prog [options]')
+    opts.add_option('-U', '--update-catalog',
+            help='Update catalog.pickle and exit',
+            nargs=0,
+            action='callback',
+            callback=option_callback,
+            )
+    opts.add_option('-t', '--tests',
+            help='Run unittests and exit',
+            nargs=0,
+            action='callback',
+            callback=option_callback,
+            )
+    opts.add_option('-r', '--run',
+            help='Run books in the range [x,y]'
+            nargs=1,
+            action='callback',
+            callback=option_callback,
+            )
+    opts.add_option('-D', '--delete-git',
+            help='Delete `.git` directories in range [x,y]',
+            nargs=1,
+            action='callback',
+            callback=option_callback,
+            )
+    opts.add_option('-u', '--update',
+            help=('Checks for changes in git directories and commits them.'
+                ' Range: [x,y]')
+            nargs=1,
+            action='callback',
+            callback=option_callback,
+            )
+    opts.parse_args()

fac1605d9a65e202187a28d54b37770f92473732
Seth Woodworth <seth@sethish.com>
2014-07-24 10:22:41 -0400

    add CantPathHere exception and adding as export

--- gitenberg/path.py
+++ gitenberg/path.py
@@ -8,11 +8,17 @@ Utilities for finding the paths to books remotely and locally
 LIBRARY_PATH = './library'
 
 
+class CantPathHereException(Exception):
+    pass
+
+
 def path_to_pg_book(book_id):
     """ turns an ebook_id into a string of paths
         this is the way PG organizes their folder paths
           4443  -> 4/4/4/4443/
     """
+    if book_id < 10:
+        raise CantPathHereException
     path = ''
     for digit in str(book_id)[:-1]:
         path = path + '{0}/'.format(digit)

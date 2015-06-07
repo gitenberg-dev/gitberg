7f008ec80d7a2b681c35dfeb992043a6d65667cc
Seth Woodworth <seth@sethish.com>
2015-06-06 23:22:45 -0400

    adds properly formatted __version__ and requirements to setup

--- setup.py
+++ setup.py
@@ -1,27 +1,45 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# from __future__ import unicode_literals
+import re
+
+from pip.req import parse_requirements
+from setuptools import find_packages
 from setuptools import setup
 
-from gitenberg import __version__
 
-files = ['*']
+with open('gitenberg/__init__.py', 'r') as fd:
+    reg = re.compile(r'__version__ = [\'"]([^\'"]*)[\'"]')
+    for line in fd:
+        m = reg.match(line)
+        if m:
+            __version__ = m.group(1)
+            break
 
 setup(name='GITenberg',
       version=__version__,
       description="A library and command for interacting with the GITenberg books project",
+      long_description=open('README.rst').read(),
+      license=open('LICENSE').read(),
       author='Seth Woodworth',
       author_email='seth@sethish.com',
       url='https://github.com/sethwoodworth/gitenberg',
       packages=['gitenberg', 'gitenberg.util'],
-      # package_data={
-      #     'gitenberg': ['gitenberg/*'],
-      #     'gitenberg.util': ['gitenberg/util/*'],
-      # },
+      # packages=find_packages(),
       scripts=['gitbook'],
-      # long_description="",
-      # classifiers=[],
-      # py_modules=['gitenberg'],
+      install_requires=[
+          'github3.py>=0.9.9',
+          'docopt>=0.6',
+          'sh>=1',
+          'Jinja2>=2.7',
+      ],
       test_suite='gitenberg.test',
+      classifiers=[
+          'Development Status :: 3 - Alpha'
+          'Intended Audience :: Developers',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.4',
+      ],
+      keywords="books gutenberg metadata",
+
       )

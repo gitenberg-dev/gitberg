79bec3d2e662d862db6ac38550fdcb669e322138
eric <eric@hellman.net>
2016-02-11 21:38:43 -0500

    add to cli

--- gitenberg/__init__.py
+++ gitenberg/__init__.py
@@ -7,7 +7,7 @@ from .book import Book
 from .clone import clone
 from .config import ConfigFile, check_config, NotConfigured
 from .library import main as library
-from .workflow import upload_all_books
+from .workflow import upload_all_books, upload_list, upload_book
 
 __title__ = 'gitberg'
 __appname__ = 'gitberg'

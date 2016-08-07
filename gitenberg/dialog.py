d402868ade097dd0ba2d9492645bbabf2b734ff5
Seth Woodworth <seth@sethish.com>
2016-08-06 22:55:44 -0400

    Fixes config generator, closes #119

--- gitenberg/dialog.py
+++ gitenberg/dialog.py
@@ -9,14 +9,16 @@ from getpass import getpass
 
 class ConfigGenerator(object):
     def __init__(self, current={}):
-        """ Generates a config file with necessary data for gitberg
+        """ Asks questions of the user and
+        generates a config file with necessary data for gitberg
+        with the responses.
+        `.answers` contains responses suitable for saving into a config
         """
         self.answers = {}
         self.current = current
         if not self.current.get('library_path', ''):
             current['library_path']= user_data_dir('gitberg', 'Free Ebook Foundation')
-        
-    
+
     def ask_generic(self, key, prompt, pwd = False):
         if pwd:
             answer = getpass( prompt )
@@ -27,15 +29,15 @@ class ConfigGenerator(object):
                 prompt = '{} >'.format(prompt)
             answer = input( prompt )
         self.answers[key] = answer if answer else self.current.get(key,None)
-    
+
     def ask_username(self):
         self.ask_generic('gh_user', "What is your GitHub username?")
 
     def ask_email(self):
-        self.ask_generic('gh_email',"What is your GitHub email?")
+        self.ask_generic('gh_email', "What is your GitHub email?")
 
     def ask_rdf_library(self):
-        self.ask_generic('rdf_library',"What is the path to your PG RDF library?")
+        self.ask_generic('rdf_library', "What is the path to your PG RDF library?")
 
     def ask_password(self):
         self.ask_generic('gh_password',
@@ -45,10 +47,7 @@ class ConfigGenerator(object):
         )
 
     def ask_library(self):
-        # Suggest the standard data dir for user's OS
         self.ask_generic( 'library_path', "What is the path to the folder where you would like to store books?")
-        # FIXME: Check and ensure that the full dir path to the folder is
-        # created and that nothing is overwritten by doing so
 
     def ask(self):
         self.ask_library()

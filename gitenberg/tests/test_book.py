gitenberg-dev/gitberg

commit 6cea4670758debf8246eb18d9a604b397967ac36

path management was too complex

gitenberg/tests/test_book.py
@@ -15,17 +15,17 @@ class TestBookPath(unittest.TestCase):
         with patch.object(gitenberg.config.appdirs, 'user_config_dir', here) as path:
             with patch('github3.login') as login:
                 self.login = login
-                self.book = Book(3456)
+                self.book = Book(1234)
 
     def test_remote_path(self):
         self.assertEqual(
             self.book.remote_path,
-            "3/4/5/3456/"
+            "1/2/3/1234/"
         )
 
     def test_local_path(self):
         self.assertTrue(
-            self.book.local_path.endswith("/3456")
+            self.book.local_path.endswith("/1234")
         )
 
     def test_remote_path_below_ten(self):

gitenberg/book.py
@@ -26,25 +26,54 @@ from .metadata.pandata import Pandata
 class Book():
     """ An index card tells you where a book lives
         `book_id` is PG's unique book id
-        `remote_path` is where it lives on PG servers
-        `local_path` is where it should be stored locally
+        `remote_path` is where it should live on PG servers
+        `srepo_name` is the name the repo should have on GitHub
+        `local_path` is where it IS stored locally
     """
 
     def __init__(self, book_id, repo_name=None, library_path='./library'):
-        if repo_name and not book_id:
-            self.repo_name = repo_name
-            book_id = repo_name.split('_')[-1]
-        else:
-            self.repo_name = None
-        self.book_id = str(book_id)
-        self.local_repo = None
-        self.github_repo = GithubRepo(self) # a side effect of this is to init the config
-        try:
-            self.library_path = config.data.get("library_path",library_path)
-        except:
-            # no config, used in tests
-            self.library_path = library_path
+        # rename to avoid confusion
+        arg_repo_name = repo_name
+        self.local_path = None
+        
+        # do config
+        self.library_path = config.get_library_path(library_path)
+        
+        # parse the inputs to figure out the book
+        if arg_repo_name and not book_id:
+            book_id = arg_repo_name.split('_')[-1]
 
+        if book_id:
+            self.book_id = str(book_id)
+            self.repo_name = get_repo_name(self.book_id)
+            self.set_existing_local_path(self.book_id)
+        else:
+            self.book_id = None
+            self.repo_name = None
+        
+        # check if there's a directory named with the arg_repo_name
+        if arg_repo_name and not self.local_path:
+            self.set_existing_local_path(arg_repo_name)
+
+        # or, check if there's a directory named with the github name
+        if self.repo_name and not self.local_path:
+            self.set_existing_local_path(self.repo_name)
+        
+        # set up the local repo
+        if self.local_path:
+            self.local_repo = LocalRepo(self.local_path)
+        else:
+            self.local_repo = None
+        
+        # set up the Github connection
+        self.github_repo = GithubRepo(self)
+    
+    def set_existing_local_path(self, name):
+        path = os.path.join(self.library_path, name)
+        if os.path.exists(path):
+            self.local_path = path
+    
+    
     def parse_book_metadata(self, rdf_library=None):
         # cloned repo
         if self.local_repo and self.local_repo.metadata_file:
@@ -79,21 +108,6 @@ class Book():
         path_parts.append(self.book_id)
         return os.path.join(*path_parts) + '/'
 
-    @property
-    def local_path(self):
-        if self.local_repo:
-            return self.local_repo.repo_path
-        local_path = os.path.join(self.library_path, self.book_id)
-        if self.repo_name:
-            named_path = os.path.join(self.library_path, self.repo_name)
-            if os.path.exists(named_path):
-                return named_path
-        return local_path
-    
-    def add_local(self):
-        if not self.local_repo:
-            self.local_repo = LocalRepo(self.local_path)
-        
     def fetch(self):
         """ just pull files from PG
         """
@@ -101,13 +115,17 @@ class Book():
         fetcher.fetch()
     
     def clone_from_github(self):
-        self.local_repo = clone(self.book_id)
-        self.repo_name = get_repo_name(self.book_id)
+        if self.local_repo: 
+            # don't need to clone the repo
+            # perhaps we should delete the repo and refresh?
+            pass
+        else:
+            self.local_repo = clone(self.repo_name)
+            self.local_path = self.local_repo.repo_path
     
     def make(self):
         """ turn fetched files into a local repo, make auxiliary files
         """
-        self.add_local()
         logging.debug("preparing to add all git files")
         num_added = self.local_repo.add_all_files()
         if num_added:

gitenberg/clone.py
@@ -12,7 +12,6 @@ from .util.catalog import get_repo_name
 clone_url_ssh_template = u"git@github.com:{org_name}/{repo_name}.git"
 
 def clone(book_repo_name, library_path=None):
-    book_repo_name = get_repo_name(book_repo_name)
     logging.info("running clone for{}".format(book_repo_name))
     vat = CloneVat(book_repo_name)
 

gitenberg/config.py
@@ -17,6 +17,17 @@ class NotConfigured(Exception):
 # static global
 data = {}
 
+def get_library_path(library_path='./library'):
+    """ load config if needed, return library path """
+    global data
+    if data == {}:
+        ConfigFile()
+    try:
+        return data.get("library_path",library_path)
+    except:
+        # no config, used in tests
+        return library_path
+
 class ConfigFile(object):
     """ A wrapper for managing creating and reading a config file
     takes (optional) appname str kwarg,

gitenberg/push.py
@@ -47,17 +47,14 @@ class GithubRepo():
     
     def create_and_push(self):
         self.create_repo()
-        self.book.add_local()
         origin = self.add_remote_origin_to_local_repo()
         origin.push(self.book.local_repo.git.refs.master)
     
     def update(self, message):
-        self.book.add_local()
         self.book.local_repo.update(message)
         self.push()
         
     def tag(self, version):
-        self.book.add_local()
         if version == "bump":
             old_version = self.book.meta._version
             if old_version:
@@ -76,8 +73,6 @@ class GithubRepo():
         
     def create_api_handler(self):
         """ Creates an api handler and sets it on self """
-        if not config.data:
-            raise config.NotConfigured
         try:
             self.github = github3.login(username=config.data['gh_user'],
                                     password=config.data['gh_password'])
@@ -192,7 +187,7 @@ class GithubRepo():
 
         return base64.b64encode(ciphertext)
         
-    def travis_key(self):        
+    def travis_key(self):  
         if self.book.local_repo:
             travis_key = self.book.local_repo.travis_key
             if travis_key:

gitenberg/tests/test_data/config.yaml
@@ -1,5 +1,5 @@
 gh_email: gh_email@example.com
 gh_password: gh_password
 gh_user: gh_user
-library_path: .
+library_path: gitenberg/tests/test_data/rdf_library
 rdf_library: gitenberg/tests/test_data/rdf_library

gitenberg/tests/test_make.py
@@ -21,7 +21,8 @@ class TestNewFileHandler(unittest.TestCase):
             with patch('github3.login') as login:
                 self.login = login
                 self.book = Book(1234)
-        self.book.local_repo = LocalRepo(os.path.join(os.path.dirname(__file__),'test_data/1234'))
+        self.book.local_path = os.path.join(os.path.dirname(__file__),'test_data/1234')
+        self.book.local_repo = LocalRepo(self.book.local_path)
         self.book.parse_book_metadata()
         self.file_maker = NewFilesHandler(self.book)
 

commit 9183881a12e350309e35a484f5c11d8ac998621a

Fix test_book
I think 6cea4670 broke this test by reshuffing what each method is
supposed to do.

gitenberg/tests/test_book.py
@@ -7,7 +7,7 @@ from mock import patch
 import gitenberg
 from gitenberg.book import Book
 
-    
+
 class TestBookPath(unittest.TestCase):
     def setUp(self):
         self.test_book_dir = 'test_book'
@@ -18,6 +18,7 @@ class TestBookPath(unittest.TestCase):
             with patch('github3.login') as login:
                 self.login = login
                 self.book = Book(1234)
+                self.fakebook = Book(1235)
 
     def test_remote_path(self):
         self.assertEqual(
@@ -40,7 +41,15 @@ class TestBookPath(unittest.TestCase):
 
     @patch('os.makedirs')
     @patch('os.chmod')
-    def test_make_local_path(self, mock_chmod, mock_makedirs):
-        self.book.set_existing_local_path(self.test_book_dir)
+    def test_make_new_local_path(self, mock_chmod, mock_makedirs):
+        self.fakebook.make_local_path()
         mock_makedirs.assert_called_once()
         mock_chmod.assert_called_once()
+
+    @patch('os.makedirs')
+    @patch('os.chmod')
+    def test_make_existing_local_path(self, mock_chmod, mock_makedirs):
+        self.book.set_existing_local_path(self.test_book_dir)
+        self.book.make_local_path()
+        mock_makedirs.assert_not_called()
+        mock_chmod.assert_not_called()
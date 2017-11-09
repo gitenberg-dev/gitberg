fb8c6096e9a9cbe0f527af12f0efbd0d8179ffb1
marc <marc@laptop>
2017-11-09 15:59:54 -0500

    Add download RDF option to config dialog
    Also update setup.py to work on debian stable, add wget

--- gitenberg/dialog.py
+++ gitenberg/dialog.py
@@ -3,9 +3,17 @@
 """ A collection of tools for interactive commandline dialogs
 """
 
+import os
+import wget
+import tarfile
+
 from appdirs import user_data_dir
 from six.moves import input
 from getpass import getpass
+from .util import util
+
+# gutenberg RDF link mirror
+RDF_URL = "http://gutenberg.readingroo.ms/cache/generated/feeds/rdf-files.tar.bz2"
 
 class ConfigGenerator(object):
     def __init__(self, current={}):
@@ -27,7 +35,7 @@ class ConfigGenerator(object):
                 prompt = '{} >'.format(prompt)
             answer = input( prompt )
         self.answers[key] = answer if answer else self.current.get(key,None)
-    
+
     def ask_username(self):
         self.ask_generic('gh_user', "What is your GitHub username?")
 
@@ -35,8 +43,27 @@ class ConfigGenerator(object):
         self.ask_generic('gh_email',"What is your GitHub email?")
 
     def ask_rdf_library(self):
-        self.ask_generic('rdf_library',"What is the path to your PG RDF library?")
 
+        # Ensure we get a y/n answer
+        shouldDownload = None
+        while shouldDownload == None:
+            answer = input("Do you have a local PG RDF library [Y/n]")
+            if answer == "y" or answer == "Y" or answer == "":
+                shouldDownload = False
+            elif answer == "n" or answer == "N":
+                shouldDownload = True
+            
+        if shouldDownload:
+            self.ask_generic('rdf_library',"What is the path to the directory where you would like to have the PG RDF library? (This is a >= 1.1GB file)")
+            if not os.path.exists(self.answers['rdf_library']):
+                os.makedirs(self.answers['rdf_library'])
+            with util.cd(self.answers['rdf_library']):   
+                bz2_rdf = wget.download(RDF_URL)
+                with tarfile.open(bz2_rdf, 'r:bz2') as out:
+                    out.extractall()
+        else:
+            self.ask_generic('rdf_library',"What is the path to your PG RDF library?")
+            
     def ask_password(self):
         self.ask_generic('gh_password',
             "What is the GitHub password for the {0} user?".format(

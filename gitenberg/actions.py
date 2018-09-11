5534c4a22ff5028537468a93698735b4326367da
eric <eric@hellman.net>
2018-09-11 14:14:59 -0400

    remove remaining travis

--- gitenberg/actions.py
+++ gitenberg/actions.py
@@ -49,14 +49,12 @@ def add_generated_cover(repo_name, tag=False):
 def refresh_repo(repo_name):
     book = get_cloned_book(repo_name)
     filemaker = NewFilesHandler(book)
-    filemaker.travis_files()
     filemaker.copy_files()
     book.add_covers()
     book.local_repo.add_all_files()
-    book.local_repo.commit('Update cover, travis files')
+    book.local_repo.commit('Update cover')
     book.github_repo.update_repo()
     book.tag()
-    book.github_repo.enable_travis()
     return book
 
 def refresh_repo_desc(repo_name):

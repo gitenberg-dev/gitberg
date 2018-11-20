6724356888fdf5da9776bbfcea7f8abf52d479ec
eric <eric@hellman.net>
2018-11-20 12:33:40 -0500

    add Rdfcache object
    from gitberg-autoupdate, adding code to list books not uploaded yet

--- gitenberg/util/catalog.py
+++ gitenberg/util/catalog.py
@@ -5,16 +5,27 @@
 """
 import csv
 import json
+import logging
 import os
 import pytz
 import re
+import shutil
+import tarfile
+import time
+
 import dateutil.parser
+import requests
 
 from .. import pg_wikipedia
 from ..config import NotConfigured
 from ..metadata.pandata import Pandata
 from ..metadata.pg_rdf import pg_rdf_to_json, htm_modified
 from ..parameters import GITHUB_ORG
+
+RDF_URL = "https://www.gutenberg.org/cache/epub/feeds/rdf-files.tar.bz2"
+RDF_PATH = "/tmp/rdf.tar.bz2" 
+# 1 day
+RDF_MAX_AGE = 60 * 60 * 24
 
 # sourced from http://www.gutenberg.org/MIRRORS.ALL
 MIRRORS = {'default': 'ftp://gutenberg.pglaf.org/mirrors/gutenberg/'}
@@ -34,6 +45,7 @@ with open(os.path.join(os.path.dirname(__file__), '../data/GITenberg_repo_list.t
     for row in csv.reader(repofile, delimiter='\t', quotechar='"'):
         repo_list.append(row)
 repo_for_pgid = {int(pgid): value for (pgid, value) in repo_list}
+last_pgid = repo_list[-1][0]
 
 missing_list = []
 with open(os.path.join(os.path.dirname(__file__), '../data/missing.tsv')) as missingfile:
@@ -107,3 +119,73 @@ class BookMetadata(Pandata):
     def pg_modified(self):
         mod_str = htm_modified(self.rdf_path)
         return utc.localize(dateutil.parser.parse(mod_str))
+
+class Rdfcache(object):
+    downloading = False
+    def __init__(self, rdf_library='./rdf_library'):
+        if rdf_library.endswith('/cache/epub'):
+            # because unzipping the archive creates ./cache/epub
+            self.rdf_library_dir = rdf_library[0:-11] 
+        else:
+            self.rdf_library_dir = rdf_library
+            
+    
+    
+    def download_rdf(self):
+        """Ensures a fresh-enough RDF file is downloaded and extracted.
+
+        Returns True on error."""
+        if self.downloading:
+            return True
+        
+        if (os.path.exists(RDF_PATH) and
+                (time.time() - os.path.getmtime(RDF_PATH)) < RDF_MAX_AGE):
+            return False
+        self.downloading = True
+        logging.info('Re-downloading RDF library from %s' % RDF_URL)
+        try:
+            shutil.rmtree(os.path.join(self.rdf_library_dir, 'cache'))
+        except OSError as e:
+            # Ignore not finding the directory to remove.
+            if e.errno != errno.ENOENT:
+                raise
+
+        try:
+            with open(RDF_PATH, 'w') as f:
+                with requests.get(RDF_URL, stream=True) as r:
+                    shutil.copyfileobj(r.raw, f)
+        except requests.exceptions.RequestException as e:
+            logging.error(e)
+            return True
+
+        try:
+            with tarfile.open(RDF_PATH, 'r') as f:
+                f.extractall(self.rdf_library_dir)
+        except tarfile.TarError as e:
+            logging.error(e)
+            try:
+                os.unlink(RDF_PATH)
+            except:
+                pass
+            return True
+        self.downloading = False
+        return False
+
+    def get_repos_to_upload(self):
+        pg_id = int(last_pgid) + 1
+        more = True
+        pg_ids = []
+        while more:
+            new_rdffile = os.path.join(
+                self.rdf_library_dir,
+                'cache',
+                'epub',
+                unicode(pg_id),
+                'pg{}.rdf'.format(pg_id)
+            )
+            if os.path.exists(new_rdffile):
+                pg_ids.append(pg_id)
+                pg_id += 1
+            else:
+                more = False
+        return pg_ids

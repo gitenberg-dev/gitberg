e021dbe7d1ddfb3591e34df83413b2a878904838
eric <eric@hellman.net>
2019-01-03 11:31:51 -0500

    add audio filetypes
    remove mp3 from filetypes not staged for git

--- gitenberg/util/filetypes.py
+++ gitenberg/util/filetypes.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 
 COMPRESSED_FILES    = ['.zip', '.rar']
-IGNORE_AUDIO_FILES     = ['.mp3', '.m4b', '.ogg', '.spx', '.wav', '.raw', '.ogg~', '.mpg', '.mpeg', '.MP3', '.mp4', '.m4a', '.wma', '.aac']
-AUDIO_FILES     = [u'mid', u'midi', 'sib', 'mus']
+IGNORE_AUDIO_FILES     = ['.m4b', '.ogg', '.spx', '.wav', '.raw', '.ogg~', '.mpg', '.mpeg', '.MP3', '.mp4', '.m4a', '.wma', '.aac']
+AUDIO_FILES     = [u'mid', u'midi', u'sib', u'mus', u'mxl']
 IMAGE_FILES     = ['png', 'jpg', 'GIF', u'gif', u'bmp', 'tiff', 'tif', 'jpeg', 'JPG', 'eps', 'PNG']
 MASTER_FILES    = ['tei', 'rst', 'txt', 'rtf', 'tex', 'TXT']
 MARKUP_FILES    = ['htm', 'html', 'xml', 'eepic', 'css', 'xp', 'svg', 'ps', 'xsl']
